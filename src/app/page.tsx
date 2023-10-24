560a34510bc508c147f43b795f0e467bd3efff96
Michaell Alavedra <f.michaell.a.m@gmail.com>
2023-10-24 16:00:28 +0000

    style(fonts): test variable font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-center'>
 			<div className='container flex flex-col items-center justify-center gap-12 px-4 py-16 '>
-				<h1 className='text-5xl font-extrabold tracking-tight sm:text-[5rem]'>
+				<h1 className='title tracking-tight'>
 					{/* <span className='text-[hsl(0,100%,100%)]'>Michaell Alavedra</span> */}
 					{developer.name}
 				</h1>
